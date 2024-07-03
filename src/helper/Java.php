e45a9acb29ace0c8cd78b622f4d953c95755631c
eavMarshall <elliottavmarshall@gmail.com>
2024-07-03 17:26:30 +1200

    Setting encoding as an option

--- src/helper/Java.php
+++ src/helper/Java.php
@@ -17,18 +17,18 @@ class Java
      * @param string $s
      * @return int
      */
-    public static function javaStringHashCode($s)
+    public static function javaStringHashCode($s, $stringEncoding = 'UTF-16')
     {
         $hash = 0;
         $bitmask32 = 0xFFFFFFFF; // 32-bit mask to emulate 32-bit int overflow behavior
         $maxInt32 = 0x7FFFFFFF; // max value for a 32-bit signed integer
-        $s = mb_convert_encoding($s, "UTF-16");
-        $n = mb_strlen($s, 'UTF-16');
+        $s = mb_convert_encoding($s, $stringEncoding);
+        $n = mb_strlen($s, $stringEncoding);
 
         for ($i = 0; $i < $n; $i++) {
             // $s[$i] accesses a single byte, mb_substr will get whole character
-            $char = mb_substr($s, $i, 1, 'UTF-16');
-            $code = mb_ord($char, 'UTF-16');
+            $char = mb_substr($s, $i, 1, $stringEncoding);
+            $code = mb_ord($char, $stringEncoding);
 
             // s[0]*31^(n-1) + s[1]*31^(n-2) + ... + s[n-1]
             $hash = (31 * $hash + $code) & $bitmask32;
