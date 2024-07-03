f46b3c3cd2b021354fdd60f2f07c2b926e655f77
eavMarshall <elliottavmarshall@gmail.com>
2024-07-03 17:43:57 +1200

    adding comment, renaming vars to match java implementation

--- src/helper/Java.php
+++ src/helper/Java.php
@@ -15,11 +15,27 @@ class Java
      * https://stackoverflow.com/questions/43482364/java-implementation-of-phps-ord-yields-different-results-for-chars-beyond-as
      *
      * @param string $s
+     * @param string $stringEncoding
      * @return int
      */
-    public static function javaStringHashCode($s, $stringEncoding = 'UTF-16')
+    public static function javaStringHashCode(string $s, string $stringEncoding = 'UTF-16'): int
     {
-        $hash = 0;
+        /*
+            // java implementation
+            public int hashCode() {
+                int h = hash;
+                if (h == 0 && value.length > 0) {
+                    char val[] = value;
+
+                    for (int i = 0; i < value.length; i++) {
+                        h = 31 * h + val[i];
+                    }
+                    hash = h;
+                }
+                return h;
+            }
+        */
+        $h = 0;
         $bitmask32 = 0xFFFFFFFF; // 32-bit mask to emulate 32-bit int overflow behavior
         $maxInt32 = 0x7FFFFFFF; // max value for a 32-bit signed integer
         $s = mb_convert_encoding($s, $stringEncoding);
@@ -28,17 +44,16 @@ class Java
         for ($i = 0; $i < $n; $i++) {
             // $s[$i] accesses a single byte, mb_substr will get whole character
             $char = mb_substr($s, $i, 1, $stringEncoding);
-            $code = mb_ord($char, $stringEncoding);
-
-            // s[0]*31^(n-1) + s[1]*31^(n-2) + ... + s[n-1]
-            $hash = (31 * $hash + $code) & $bitmask32;
+            $asciiCode = mb_ord($char, $stringEncoding);
+            // h = 31 * h + val[i];
+            $h = (31 * $h + $asciiCode) & $bitmask32;
         }
 
         // adjust for negative values as Java hashCode can return negative numbers
-        if ($hash > $maxInt32) {
-            $hash -= 0x100000000;
+        if ($h > $maxInt32) {
+            $h -= 0x100000000;
         }
 
-        return $hash;
+        return $h;
     }
 }
